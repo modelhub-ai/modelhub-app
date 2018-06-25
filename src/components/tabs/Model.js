2bddeaf5a077e41c0d40bc2afe76f44fc42d5254
Ahmed Hosny <i.ahmedhosny@gmail.com>
2018-06-25 15:11:38 -0400

    allows other model viewers in model

--- src/components/tabs/Model.js
+++ src/components/tabs/Model.js
@@ -18,25 +18,30 @@ class Model extends Component {
       left: 16,
       fontSize: 11,
     });
-    return config.viewer === 'netron' ? (
-      <div>
-        <iframe
-          title={'netron viewer'}
-          height={'700'}
-          width={'100%'}
-          src={data.url + data.viewer}
-          frameBorder={'0'}
-        />
-        <GDiv>
-          Powered by{' '}
-          <a target={'_blank'} href={'https://github.com/lutzroeder/Netron'}>
-            Netron
-          </a>.
-        </GDiv>
-      </div>
-    ) : (
-      'This viewer is not supported.'
-    );
+    let modelContent;
+    if (config.viewer === 'netron') {
+      modelContent = (
+        <div>
+          <iframe
+            title={'netron viewer'}
+            height={'700'}
+            width={'100%'}
+            src={data.url + data.viewer}
+            frameBorder={'0'}
+          />
+          <GDiv>
+            Powered by{' '}
+            <a target={'_blank'} href={'https://github.com/lutzroeder/Netron'}>
+              Netron
+            </a>.
+          </GDiv>
+        </div>
+      );
+    } else if (config.viewer !== 'netron') {
+      // others go here.
+      modelContent = 'This viewer is not supported.';
+    }
+    return <div>{modelContent}</div>;
   }
 }
 
