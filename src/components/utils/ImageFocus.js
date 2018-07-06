c7d89e2f4d0b8b72551f37193f39706714e0a4c3
Ahmed Hosny <i.ahmedhosny@gmail.com>
2018-07-05 22:08:58 -0400

    adds ImageFocus

---
+++ src/components/utils/ImageFocus.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import glamorous from 'glamorous';
+
+/**
+ * ImageFocus Class
+ * @extends Component
+ */
+class ImageFocus extends Component {
+  /**
+   * Renders ImageFocus
+   * @return {ReactElement}
+   */
+  render() {
+    const {src} = this.props;
+    const GDiv = glamorous.div({
+      height: 500,
+      backgroundColor: '#eeeeee',
+      border: '1px solid #000',
+      background: 'url(' + src + ') center center no-repeat',
+      backgroundSize: '50%', // 'contain',
+    });
+    return <GDiv />;
+  }
+}
+
+ImageFocus.propTypes = {
+  src: PropTypes.string.isRequired,
+};
+
+export default ImageFocus;
