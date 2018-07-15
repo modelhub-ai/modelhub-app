a612642ae973dbd5f03c5b8eb9b63e94b3504ffb
Ahmed Hosny <i.ahmedhosny@gmail.com>
2018-07-15 19:28:47 -0400

    changes models to cxr

--- src/components/tabs/Installation.js
+++ src/components/tabs/Installation.js
@@ -1,9 +1,8 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import Typography from '@material-ui/core/Typography';
-import TextPair from '../utils/TextPair';
-import glamorous from 'glamorous';
-import NoSupport from '../utils/NoSupport';
+// import Typography from '@material-ui/core/Typography';
+// import TextPair from '../utils/TextPair';
+// import glamorous from 'glamorous';
 
 /**
  * Installation Class
@@ -15,75 +14,8 @@ class Installation extends Component {
    * @return {ReactElement}
    */
   render() {
-    const {script} = this.props.fetches.config;
-    const GTypography = glamorous(Typography)({
-      marginBottom: '25px !important',
-    });
-    let installationContent = [];
-    script.forEach((scriptInstance, index) => {
-      const {variables} = scriptInstance;
-      if (scriptInstance.type === 'bash') {
-        installationContent.push(
-          <div key={index}>
-            <GTypography variant="headline">
-              Install using bash script.
-            </GTypography>
-            <TextPair
-              title={
-                <div>
-                  1. Install{' '}
-                  <a
-                    target={'_blank'}
-                    href={'https://docs.docker.com/install/'}
-                  >
-                    Docker
-                  </a>. (CE is sufficient)
-                </div>
-              }
-              spacer={10}
-            />
-            <TextPair
-              title={'2. Download the bash script.'}
-              body={`$ curl -O https://raw.githubusercontent.com/modelhub-ai/modelhub/master/start_scripts/${
-                variables.file_name
-              }`}
-              spacer={10}
-              code
-            />
-            <TextPair
-              title={'3. Change permissions.'}
-              body={`$ chmod +x ${variables.file_name}`}
-              spacer={10}
-              code
-            />
-            <TextPair title={'4. Run.'} spacer={10} />
-            <TextPair
-              title={'For the basic test drive version:'}
-              body={`$ sudo ./${variables.file_name}`}
-              spacer={10}
-              code
-            />
-            <TextPair
-              title={'For the jupyter notebook version:'}
-              body={`$ sudo ./${variables.file_name} -e`}
-              spacer={10}
-              code
-            />
-            <TextPair
-              title={'To explore the docker:'}
-              body={`$ sudo ./${variables.file_name} -b`}
-              spacer={10}
-              code
-            />
-          </div>
-        );
-      } else if (scriptInstance.type !== 'bash') {
-        installationContent.push(
-          <NoSupport key={index} message={'installation type'} />
-        );
-      }
-    });
-    return <div>{installationContent}</div>;
+    //
+    return <div>{'installation'}</div>; // {installationContent}
   }
 }
 
@@ -93,3 +25,70 @@ Installation.propTypes = {
 };
 
 export default Installation;
+
+// let installationContent = [];
+// script.forEach((scriptInstance, index) => {
+//   const {variables} = scriptInstance;
+//   if (scriptInstance.type === 'bash') {
+//     installationContent.push(
+//       <div key={index}>
+//         <GTypography variant="headline">
+//           Install using bash script.
+//         </GTypography>
+//         <TextPair
+//           title={
+//             <div>
+//               1. Install{' '}
+//               <a
+//                 target={'_blank'}
+//                 href={'https://docs.docker.com/install/'}
+//               >
+//                 Docker
+//               </a>. (CE is sufficient)
+//             </div>
+//           }
+//           spacer={10}
+//         />
+//         <TextPair
+//           title={'2. Download the bash script.'}
+//           body={`$ curl -O https://raw.githubusercontent.com/modelhub-ai/modelhub/master/start_scripts/${
+//             variables.file_name
+//           }`}
+//           spacer={10}
+//           code
+//         />
+//         <TextPair
+//           title={'3. Change permissions.'}
+//           body={`$ chmod +x ${variables.file_name}`}
+//           spacer={10}
+//           code
+//         />
+//         <TextPair title={'4. Run.'} spacer={10} />
+//         <TextPair
+//           title={'For the basic test drive version:'}
+//           body={`$ sudo ./${variables.file_name}`}
+//           spacer={10}
+//           code
+//         />
+//         <TextPair
+//           title={'For the jupyter notebook version:'}
+//           body={`$ sudo ./${variables.file_name} -e`}
+//           spacer={10}
+//           code
+//         />
+//         <TextPair
+//           title={'To explore the docker:'}
+//           body={`$ sudo ./${variables.file_name} -b`}
+//           spacer={10}
+//           code
+//         />
+//       </div>
+//     );
+//   } else if (scriptInstance.type !== 'bash') {
+//     installationContent.push(
+//       <GTypography key={index} variant="headline">
+//         Installation type is not supported by this frontend.
+//       </GTypography>
+//     );
+//   }
+// });
